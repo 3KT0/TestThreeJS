f8b69a87aa75f01a9a3a2efdd11c57b76c707ef1
3KT0 <olingertheo@aol.fr>
2024-03-20 15:18:16 +0100

    mesh animations
    remove geometry by mesh loadObject
    remove const and change mesh by mesh.material
    adding const mixer, clock
    adding a const in function

--- script.js
+++ script.js
@@ -4,10 +4,9 @@ import GLTFLoader from 'gltfloader'
 
 const   scene = new THREE.Scene()
 const   camera = new THREE.PerspectiveCamera(70, iw / ih)
-const   geometry = await GLTFLoader.loadGeometry('bibi.glb')
+const   mesh = await GLTFLoader.loadObject('bibi.glb','bibi')
 const   texture = new THREE.TextureLoader().load('bibi.png')
-const   material = new THREE.MeshPhongMaterial( {map: texture, shininess: 0} )
-const   mesh = new THREE.Mesh(geometry, material)
+mesh.material = new THREE.MeshPhongMaterial( {map: texture, shininess: 0} )
 
 const   light = new THREE.PointLight(0xeeeeee)
 
@@ -19,10 +18,17 @@ light.position.set(0, 3, 4)
 
 const   renderer = new THREE.WebGLRenderer({canvas})
 
+const   mixer = new THREE.AnimationMixer( mesh )
+mixer.clipAction( mesh.animations[ 0 ] ).setDuration( 5 ).play()
+
+const clock = new THREE.Clock()
+
 loop()
 
 function loop() {
     requestAnimationFrame(loop)
+    const dt = clock.getDelta()
+    mixer.update(dt)
     mesh.rotation.y += 0.001
     mesh.rotation.x += 0
     renderer.render(scene, camera)
