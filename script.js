777ab6754638185248a75efb4032a168190983be
3KT0 <olingertheo@aol.fr>
2024-03-20 15:37:12 +0100

    Skinning and morphing
    adding mesh children.material + oscillations moves

--- script.js
+++ script.js
@@ -5,9 +5,8 @@ import GLTFLoader from 'gltfloader'
 const   scene = new THREE.Scene()
 const   camera = new THREE.PerspectiveCamera(70, iw / ih)
 const   mesh = await GLTFLoader.loadObject('bibi2.glb','bibi')
-console.log(mesh)
 const   texture = new THREE.TextureLoader().load('bibi.png')
-mesh.material = new THREE.MeshPhongMaterial( {map: texture, shininess: 0} )
+mesh.children[1].material = new THREE.MeshPhongMaterial( {map: texture, shininess: 0} )
 
 const   light = new THREE.PointLight(0xeeeeee)
 
@@ -20,18 +19,20 @@ light.position.set(0, 3, 4)
 const   renderer = new THREE.WebGLRenderer({canvas})
 
 const   mixer = new THREE.AnimationMixer( mesh )
-mixer.clipAction( mesh.animations[ 0 ] ).setDuration( 5 ).play()
+mixer.clipAction( mesh.animations[ 0 ] ).setDuration( 4 ).play()
+mixer.clipAction( mesh.animations[ 1 ] ).setDuration( 4 ).play()
 
 const clock = new THREE.Clock()
+let t = 0
 
 loop()
 
 function loop() {
     requestAnimationFrame(loop)
     const dt = clock.getDelta()
+    t += dt
     mixer.update(dt)
-    mesh.rotation.y += 0
-    mesh.rotation.x += 0
+    mesh.rotation.y = Math.cos(t*Math.PI/4)/2
     renderer.render(scene, camera)
 }
 
